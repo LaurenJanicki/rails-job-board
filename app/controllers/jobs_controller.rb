a41b00f5a98ad4808718ba52cb5bca6c9d79806b
Lauren Janicki <lauren.a.janicki@gmail.com>
2016-01-23 14:18:53 -0800

    Add form and connect to database

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,24 @@
 class JobsController < ApplicationController
   def index
   end
+
+  def new
+    @job = Job.new
+    render :new
+  end
+
+  def create
+    Job.create(job_params)
+    # or
+    # @job.new(job_params)
+    # @job.save
+    redirect_to jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+
 end
